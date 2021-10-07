524e735ee9b0261ee09769f1a3748aeb5c5275e9
mhufflep <mhufflep@student.21-school.ru>
2021-10-07 22:55:24 +0300

    fix 08

--- new/day08/ex02/main.cpp
+++ new/day08/ex02/main.cpp
@@ -7,9 +7,9 @@ int main()
 
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << "top: " << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << "size: " << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
@@ -21,16 +21,36 @@ int main()
 	MutantStack<int>::reverse_iterator rbeg = mstack.rbegin();
 	MutantStack<int>::reverse_iterator rend = mstack.rend();
 
+	std::cout << "direct: ";
 	while (beg != end) {
-		std::cout << *beg;
+		std::cout << *beg << " ";
 		++beg;
 	}
-
 	std::cout << std::endl;
 
+	std::cout << "reverse: ";
 	while (rbeg != rend) {
-		std::cout << *rbeg;
+		std::cout << *rbeg << " ";
 		++rbeg;
+	}
+	std::cout << std::endl;
+
+
+	MutantStack<int> mstack2;
+
+	std::cout << "empty: " << mstack2.empty() << std::endl;
+	mstack2.push(4);
+	mstack2.push(2);
+	mstack2.push(2);
+	mstack2.push(1);
+
+	MutantStack<int>::iterator beg2 = mstack2.begin();
+	MutantStack<int>::iterator end2 = mstack2.end();
+
+	std::cout << "direct: ";
+	while (beg2 != end2) {
+		std::cout << *beg2 << " ";
+		++beg2;
 	}
 	std::cout << std::endl;
 
